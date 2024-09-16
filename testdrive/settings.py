8908a957a0fd7bb0b8b1bf89c760f7969c9fa47e
Marin Nikolli <sibalonat@gmail.com>
2024-09-16 17:05:17 +0200

    fix: products to installed apps

--- testdrive/settings.py
+++ testdrive/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'users',	# Add users app to the list of installed apps
+    'products',	# Add products app to the list of installed apps
 ]
 
 MIDDLEWARE = [
