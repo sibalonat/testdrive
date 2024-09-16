005feb964bfc0fe85f5f18b340badf0e35db7e27
Marin Nikolli <sibalonat@gmail.com>
2024-09-16 17:53:30 +0200

    fix: remove media from gitignore

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
-from .views import ProductGenericApiView
+from .views import ProductGenericApiView, FileUploadView
 urlpatterns = [
     path('products', ProductGenericApiView.as_view()),
     path('products/<str:pk>', ProductGenericApiView.as_view()),
-]
+    path('upload', FileUploadView.as_view()),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 from .serializers import ProductSerializer
 from .models import Product
 from rest_framework.response import Response
 from rest_framework import exceptions, status, generics, mixins
 from users.authentication import JWTAuthentication
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from testdrive.pagination import CustomPageNumberPagination
 
@@ -44,4 +48,22 @@ class ProductGenericApiView(
     def delete(self, request, pk=None):
         self.destroy(request, pk)
         return Response({'message': 'User deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    
+class FileUploadView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    # parser_classes = ['multipart/form-data']
+    parser_classes = (MultiPartParser, )
+    
+    def post(self, request):
+        file = request.FILES.get('image')
+        # file_name = default_storage.save(file.name, ContentFile(file.read()))
+        file_name = default_storage.save(file.name, file)
+        url = default_storage.url(file_name)
+        # if not file:
+        #     raise exceptions.ValidationError({'file': 'File is required'})
+        
+        return Response({
+            'url': 'http://localhost:8000/api' + url
+        }, status=status.HTTP_201_CREATED)  
  
