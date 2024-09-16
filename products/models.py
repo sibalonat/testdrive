71838d2397bd076be16bb10938e71bf179355efd
Marin Nikolli <sibalonat@gmail.com>
2024-09-16 17:04:00 +0200

    fix: permission for the file

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
